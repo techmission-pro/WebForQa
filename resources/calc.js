38fef08cc83f7ceca71cf9db55b4b8ce0b5991ef
SerhiiChebukin <znayka29011964@gmail.com>
2022-12-07 15:50:24 +0200

    added comment 1a

--- resources/calc.js
+++ resources/calc.js
@@ -1,7 +1,7 @@
-document.querySelector (".calc-calculate"). addEventListener ("click", function  () {
+document.querySelector (".calc-calculate"). addEventListener ("click", function ()  {
 const nam1= + document.querySelector("[name=nam1]").value;
 const nam2= + document.querySelector("[name=nam2]").value;
 const sum=nam1+nam2
-document.querySelector(".calc-result"). innerText=sum;
+document.querySelector(".calc-result"). value =sum;
 });
 
