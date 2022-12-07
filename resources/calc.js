050dea8dd2721a319249ebc0d0f70f763098ec37
Elena <elena.danilchenko88@gmail.com>
2022-12-08 00:07:23 +0100

    Calculator of two numbers with different operations, auto-update of the result in the calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,35 @@
-document.querySelector('.calc-calculate').addEventListener('click',function () {
+function calculate () {
 const num1 = +document.querySelector ('[name="num1"]').value;
 const num2 = +document.querySelector ('[name="num2"]').value;
+const oper = document.querySelector ('[name="oper"]').value;
 
-const sum = num1 + num2;
+let result;
 
-document.querySelector('.calc-result').value = sum;
+if (oper ==='plus') {
+	result = num1 + num2;
+} else if (oper === 'minus') {
+	result = num1 - num2;
+} else if (oper === 'multiple') {
+	result = num1 * num2;
+} else if (oper === 'divide') {
+	result = num1 / num2;
+} else {
+	console.error('Wrong oper!');
+	alert('Wrong operation is selected');
+}
+
+document.querySelector('.calc-result').value = result;
+}
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+    calculate();	
+});
+document.querySelector('[name="oper"]').addEventListener('change', function(){
+	calculate();
+});
+document.querySelector('[name="num1"]').addEventListener('input', function(){
+    calculate();
+});
+	
+document.querySelector('[name="num2"]').addEventListener('input', function(){
+    calculate();
 });
