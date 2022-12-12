95d1cfb523446bcb72ca76caa05c37618942e669
DenysSkorobogatov <sdenvik@gmail.com>
2022-12-12 20:45:49 +0200

    Added new operations to the calculator.

--- resources/calc.js
+++ resources/calc.js
@@ -1,7 +1,20 @@
 document.querySelector(".calc-calculate").addEventListener("click", function () {
 	const num1= +document.querySelector('[name="num1"]').value;
 	const num2= +document.querySelector('[name="num2"]').value;
-	const sum = num1 + num2;
-	document.querySelector(".calc-result").innerText = sum; 
+	const op= document.querySelector('[name="op"]').value;
+	
+	let result;
+	
+	if (op === 'plus') {
+		result = num1 + num2;
+    } else if (op === 'minus') {
+		result =num1 - num2;
+	} else if (op === 'multiple') {
+		result = num1 * num2;
+    } else if (op === 'divide') {
+    	result = num1 / num2;
+	}
+	
+	document.querySelector(".calc-result") .value=result; 
 
 });
